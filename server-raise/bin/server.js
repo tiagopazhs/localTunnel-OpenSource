7d0372cd3fc13c3f491fc489b830969aabeb8edc
tiagopazhs <tiagopaz.hs@gmail.com>
2023-02-08 15:18:39 -0300

    remove optimist dependencie

--- server-raise/bin/server.js
+++ server-raise/bin/server.js
@@ -1,34 +1,12 @@
-const optimist = require('optimist');
-
 const CreateServer = require('../server.js');
 
-const argv = optimist
-    .usage('Usage: $0 --port [num]')
-    .options('secure', {
-        default: false,
-        describe: 'use this flag to indicate proxy over https'
-    })
-    .options('port', {
-        default: '80',
-        describe: 'listen on this port for outside requests'
-    })
-    .options('address', {
-        default: '0.0.0.0',
-        describe: 'IP address to bind to'
-    })
-    .options('domain', {
-        describe: 'Specify the base domain name. This is optional if hosting localtunnel from a regular example.com domain. This is required if hosting a localtunnel server from a subdomain (i.e. lt.example.dom where clients will be client-app.lt.example.come)',
-    })
-    .options('max-sockets', {
-        default: 10,
-        describe: 'maximum number of tcp sockets each client is allowed to establish at one time (the tunnels)'
-    })
-    .argv;
-
-if (argv.help) {
-    optimist.showHelp();
-    process.exit();
-}
+const argv = {
+    port: 3006,
+    address: '0.0.0.0',
+    secure: false,
+    domain: undefined,
+    'max-sockets': 10,
+};
 
 const server = CreateServer({
     max_tcp_sockets: argv['max-sockets'],
@@ -55,4 +33,3 @@ process.on('uncaughtException', (err) => {
 process.on('unhandledRejection', (reason, promise) => {
     console.error(reason);
 });
-
