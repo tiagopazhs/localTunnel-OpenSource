bf3020fb2f7c6226fb64465a4427419260761dc1
tiagopazhs <tiagopaz.hs@gmail.com>
2023-02-27 11:21:19 -0300

    change port at config file

--- server-raise/src/config/config.js
+++ server-raise/src/config/config.js
@@ -1,8 +1,8 @@
 const parameters = {
-    port: 3006,
+    port: 3007,
     address: '0.0.0.0',
     secure: false,
-    host: 'localhost:3006',
+    host: 'localhost:3007',
     domain: undefined,
     landingPage: 'Localtunnel server is running!!! Use a Client Application to send a requisition with query[new] and create a new tunnel.',
     maxsockets: 10,
