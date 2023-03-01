43fbebc12c6a9fe6c653e60fda953d6ee522fd00
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-01 14:25:22 -0300

    revision of audit Crud

--- server-raise/src/controllers/audit.controller.js
+++ server-raise/src/controllers/audit.controller.js
@@ -1,41 +1,34 @@
 const TunnelAudit = require('../models/tunnel-audit.model');
 
-exports.getAuditPanel = async (req, res) => {
-  res.json("Choose your router: /tunnels-list => General tunnels status. /tunnel-status/:tunnelId => Status of your specific tunnel.");
-}
-
 exports.getAllAudits = async (req, res) => {
-  let allAudits = ""
-  if (allAudits.length === 0) allAudits = "There aren't audits."
-  res.json(allAudits)
-}
-
-exports.getTunnelAudit = async (req, res) => {
-  let id = req.params.id
   try {
-    const currentId = await TunnelAudit.findOne({ tunnelId: id })
-    if (!id) {
-      res.status(422).json({ message: 'Tunnel not found.' })
-      return
-    }
-    res.status(200).json(currentId)
+    const auditData = await TunnelAudit.find()
+    res.status(200).json(auditData)
   } catch (error) {
     res.status(500).json({ error: error })
   }
 }
 
-exports.putTunnelAudit = async (req, res) => {
-  let {idField, creationField, openField} = req.body
-  const auditTunnelUpdate = {idField, creationField, openField}
-
-  let id = req.params.id
+exports.postTunnelAudit = async (req, res) => {
+  let tunnelId = req.body.tunnelId
+  let creationDate = req.body.creationDate
+  let open = req.body.open
+  const auditCreate = new TunnelAudit({ tunnelId, creationDate, open })
   try {
-    const currentProccesId = await TunnelAudit.updateOne({tunnelId:idField}, auditTunnelUpdate)
-    if (currentProccesId.matchedCount === 0) {
-      res.status(422).json({ message: 'Tunnel was not updated.' })
-      return
-    }
-    res.status(200).json(auditTunnelUpdate)
+    await auditCreate.save()
+    return res.json({ msg: "Audit created." })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Failed to create tunnel audit.' })
+  }
+}
+
+exports.getTunnelAudit = async (req, res) => {
+  const tunnelId = req.params.id
+  try {
+    const auditData = await TunnelAudit.findOne({tunnelId: tunnelId})
+    if(!auditData) return res.status(422).json({message: 'Tunnel not found'})
+    res.status(200).json(auditData)
   } catch (error) {
     res.status(500).json({ error: error })
   }
