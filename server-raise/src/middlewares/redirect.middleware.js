ed8380a4fdf7fcfb6db8fb0c61c5b8185fd5f0a7
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-03 09:15:10 -0300

    edit redirect middleware

--- server-raise/src/middlewares/redirect.middleware.js
+++ server-raise/src/middlewares/redirect.middleware.js
@@ -1,6 +1,13 @@
+const { parameters } = require('../config/config')
+const { hasSubdomain } = require('../utils/subdomain.util')
+
 const redirect = (req, res, next) => {
-    if (req.path !== '/catalog' && req.path !== '/audit' && req.path !== '/' ) {
+    console.log('Received request' + parameters.marker)
+    if (req.path !== '/catalog' && req.path !== '/audit' && req.path !== '/landing' && req.path !== '/' ) {
         req.url = '/'
+    }
+    if (req.originalUrl !== '/?new' && !hasSubdomain(req.headers.host) && req.originalUrl === '/') {
+        res.redirect('/landing')
     }
     next();
 }
