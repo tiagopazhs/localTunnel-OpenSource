67bcd6c29b321ed98ef2f9e58998bf6d60908798
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-03 12:32:56 -0300

    edit the form to use id

--- server-raise/src/controllers/tunnel.controller.js
+++ server-raise/src/controllers/tunnel.controller.js
@@ -1,12 +1,11 @@
 const { parameters } = require('../config/config')
 const { hri } = require('human-readable-ids');
 const { newClient } = require('../services/tunnel.service')
-const auditLog = require('../utils/audit.util')
 
 exports.getStartTunnel = async (req, res) => {
-    let reqId = req.originalUrl.slice(1);
+
+    let reqId = req.originalUrl.split("/")[1]
     if (reqId === "?new") reqId = await hri.random();
-    auditLog(reqId, "create connection")
 
     const info = await newClient(reqId);
 
