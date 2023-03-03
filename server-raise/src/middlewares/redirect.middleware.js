97cf7d7ab428fdfbcedecf22460217ac320c33e2
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-03 09:41:54 -0300

    revision of redirect middleware to router

--- server-raise/src/middlewares/redirect.middleware.js
+++ server-raise/src/middlewares/redirect.middleware.js
@@ -3,7 +3,7 @@ const { hasSubdomain } = require('../utils/subdomain.util')
 
 const redirect = (req, res, next) => {
     console.log('Received request' + parameters.marker)
-    if (req.path !== '/catalog' && req.path !== '/audit' && req.path !== '/landing' && req.path !== '/' ) {
+    if (!req.path.startsWith('/catalog') && !req.path.startsWith('/audit') &&!req.path.startsWith('/landing') && req.path !== '/' ) {
         req.url = '/'
     }
     if (req.originalUrl !== '/?new' && !hasSubdomain(req.headers.host) && req.originalUrl === '/') {
