d190e66dbe2c76bc6200c608be4fca81bf6cf574
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-03 15:40:25 -0300

    revision of redirect middleware

--- server-raise/src/middlewares/redirect.middleware.js
+++ server-raise/src/middlewares/redirect.middleware.js
@@ -3,14 +3,14 @@ const { hasSubdomain } = require('../utils/subdomain.util')
 
 const redirect = (req, res, next) => {
     console.log(`Received request ${parameters.marker} http://...${req.url} `)
-    if (!req.path.startsWith('/catalog') && !req.path.startsWith('/audit') &&!req.path.startsWith('/landing') &&!req.path.startsWith('/tunnel') || req.path === '/' ) {
+    if (!req.path.startsWith('/catalog') && !req.path.startsWith('/audit') && !req.path.startsWith('/landing') && !req.path.startsWith('/tunnel') || req.path === '/') {
         let id = req.path
         res.redirect(`/tunnel${id}`)
-    }
-    if (req.originalUrl !== '/?new' && !hasSubdomain(req.headers.host) && req.originalUrl === '/') {
+    } else if (req.originalUrl !== '/?new' && !hasSubdomain(req.headers.host) && req.originalUrl === '/') {
         res.redirect('/landing')
+    } else {
+        next();
     }
-    next();
 }
 
-module.exports = redirect
+module.exports = redirect;
