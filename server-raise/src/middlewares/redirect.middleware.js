654b4ca8d9b21020139bcabf2c84faa40b26573b
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-06 10:48:24 -0300

    redirect midleware optimization

--- server-raise/src/middlewares/redirect.middleware.js
+++ server-raise/src/middlewares/redirect.middleware.js
@@ -4,19 +4,21 @@ const { hasSubdomain, isRegistered } = require('../utils/subdomain.util')
 function redirect(req, res, next) {
 
     console.log(`Received request ${parameters.marker} http://...${req.url} `)
+
     if (!hasSubdomain(req.headers.host) && req.originalUrl === '/') {
-        res.redirect('/landing')
-    } else if ( !isRegistered(req.path) || req.path === '/') {
-        let id = req.path
-        res.redirect(`/tunnel${id}`)
-    } else {
-        next();
+        return res.redirect('/landing')
     }
-    
+
+    if (!isRegistered(req.path) || req.path === '/') {
+        let id = req.path
+        return res.redirect(`/tunnel${id}`)
+    }
+
+    return next();
+
 }
 
 module.exports = redirect;
 
 
 
-    
