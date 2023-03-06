820bccc958d3055e6587e98c435092a52566ba47
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-06 08:45:58 -0300

    revision of landing page at redirect.midleware

--- server-raise/src/middlewares/redirect.middleware.js
+++ server-raise/src/middlewares/redirect.middleware.js
@@ -4,14 +4,19 @@ const { hasSubdomain, isRegistered } = require('../utils/subdomain.util')
 function redirect(req, res, next) {
 
     console.log(`Received request ${parameters.marker} http://...${req.url} `)
-    if ( !isRegistered(req.path) || req.path === '/') {
+    if (!hasSubdomain(req.headers.host) && req.originalUrl === '/') {
+        res.redirect('/landing')
+    } else if ( !isRegistered(req.path) || req.path === '/') {
         let id = req.path
         res.redirect(`/tunnel${id}`)
-    } else if (req.originalUrl !== '/?new' && !hasSubdomain(req.headers.host) && req.originalUrl === '/') {
-        res.redirect('/landing')
     } else {
         next();
     }
+    
 }
 
 module.exports = redirect;
+
+
+
+    
