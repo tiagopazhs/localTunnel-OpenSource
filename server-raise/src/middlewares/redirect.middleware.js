8318246b697e38ef3b4e06933befe3ea6d80d6da
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-16 14:02:25 -0300

    add async await to every midlewares

--- server-raise/src/middlewares/redirect.middleware.js
+++ server-raise/src/middlewares/redirect.middleware.js
@@ -1,15 +1,15 @@
 const { parameters } = require('../config/config')
 const { urlLog, hasSubdomain, isRegistered } = require('../utils/handle-url.util')
 
-function redirect(req, res, next) {
+async function redirect(req, res, next) {
 
-    console.log(`Received request ${parameters.marker} ${urlLog(req)}`)
+    console.log(`Received request ${parameters.marker} ${await urlLog(req)}`)
 
-    if (!hasSubdomain(req.headers.host) && req.originalUrl === '/') {
+    if (!await hasSubdomain(req.headers.host) && req.originalUrl === '/') {
         return res.redirect('/landing')
     }
 
-    if (!isRegistered(req.path) || req.path === '/') {
+    if (! await isRegistered(req.path) || req.path === '/') {
         let id = req.path
         return res.redirect(`/tunnel${id}`)
     }
--- server-raise/src/middlewares/tunnel.middleware.js
+++ server-raise/src/middlewares/tunnel.middleware.js
@@ -2,28 +2,28 @@ const { getClient } = require('../services/tunnel.service')
 const { getId } = require('../utils/handle-url.util')
 
 const tunnelMiddleware = async (req, res, next) => {
-    const clientId = getId(req.headers.host);
-    if (!clientId) {
-        req.url = '/'
-        return next();
-    }
+  const clientId = await getId(req.headers.host);
+  if (!clientId) {
+    req.url = '/'
+    return next();
+  }
 
-    const client = await getClient(clientId);
-    if (!client) {
-        res.statusCode = 404;
-        res.end('Tunnel not found');
-        return;
-    }
+  const client = await getClient(clientId);
+  if (!client) {
+    res.statusCode = 404;
+    res.end('Tunnel not found');
+    return;
+  }
 
-    await client.handleRequest(req, res)
-          .then(r => {
-            return r
-          })
-          .catch((error) => {
-            console.error('An error occurred while resolving the promise:', error);
-            res.statusCode = 500;
-            res.end();
-          });
+  await client.handleRequest(req, res)
+    .then(r => {
+      return r
+    })
+    .catch((error) => {
+      console.error('An error occurred while resolving the promise:', error);
+      res.statusCode = 500;
+      res.end();
+    });
 
 }
 
