34578022c162bae0aa2a1d53994ab6ca9ffc12e1
tiagopazhs <tiagopaz.hs@gmail.com>
2023-03-30 20:47:57 -0300

    feat: Create avaiable tcp port validation

--- server-raise/src/services/tunnel.service.js
+++ server-raise/src/services/tunnel.service.js
@@ -3,9 +3,44 @@ const TunnelAgent = require('./agent.service');
 const auditLog = require('../utils/audit.util')
 const catalogLog = require('../utils/catalog.util')
 const Clients = new Map()
+const Net = require('net');
+
+async function isOpenPort(id, port) {
+    const server = new Net.Server();
+    let testPortResult = true
+    try {
+        testPortResult = await new Promise((resolve) => {
+            server.on('error', async (err) => {
+                if (err.code === 'EADDRINUSE') resolve(false)
+                else await auditLog(id, err)
+            });
+            server.listen(port, () => {
+                server.close()
+                resolve(true)
+            });
+        });
+    } catch (err) {
+        await auditLog(id, err)
+    }
+    return testPortResult;
+}
+
+async function checkPort(id) {
+    const ports = [40051, 40052, 40053];
+    for (let i = 0; i < ports.length; i++) {
+        await auditLog(id, `Checking port ${ports[i]}`)
+        if (await isOpenPort(id, ports[i])) {
+            await auditLog(id, `Port ${ports[i]} is available`)
+            return ports[i];
+        }
+    }
+    await auditLog(id, `All ports are in use`)
+    return null;
+}
 
 async function newClient(id) {
-    const agent = new TunnelAgent({ clientId: id });
+    const openPort = await checkPort(id)
+    const agent = new TunnelAgent({ clientId: id, tcpPort: openPort });
     const client = new Client({ id, agent });
     Clients[id] = client;
     const { port } = await agent.listen();
